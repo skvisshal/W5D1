2de18ba7e8c9c06cec527f279921d3e10aa9ce0a
Visshal Suresh <skvisshal@gmail.com>
2021-11-01 14:45:18 -0400

    Finished Int set

--- visshal_andy_hash_map_project/lib/p01_int_set.rb
+++ visshal_andy_hash_map_project/lib/p01_int_set.rb
@@ -38,18 +38,22 @@ class IntSet
   end
 
   def insert(num)
+    self[num] << num
   end
 
   def remove(num)
+    self[num].delete(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -58,7 +62,7 @@ class IntSet
 end
 
 class ResizingIntSet
-  attr_reader :count
+  attr_reader :count, :store
 
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
@@ -66,18 +70,26 @@ class ResizingIntSet
   end
 
   def insert(num)
+    if !self.include?(num)
+      self[num] << num
+      @count += 1
+      resize! if @count >= @store.length 
+    end
   end
 
   def remove(num)
+    @count -= 1 if self[num].delete(num) != nil
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -85,5 +97,12 @@ class ResizingIntSet
   end
 
   def resize!
+      temp_store = Array.new(num_buckets * 2) { Array.new }
+      @store.each do |sub_arr|
+        sub_arr.each do |ele|
+          temp_store[ele % temp_store.length] << ele
+        end
+      end
+      @store = temp_store
   end
 end
