0e84166b5d10abd4e2643b5b356611b5c93ed923
AndyAYu <andyhkgw@gmail.com>
2021-11-01 17:02:09 -0400

    final commit

--- visshal_andy_hash_map_project/lib/p05_hash_map.rb
+++ visshal_andy_hash_map_project/lib/p05_hash_map.rb
@@ -1,6 +1,8 @@
 require_relative 'p04_linked_list'
 
 class HashMap
+  extend Enumerable
+
   attr_accessor :count
 
   def initialize(num_buckets = 8)
@@ -9,18 +11,33 @@ class HashMap
   end
 
   def include?(key)
+    bucket(key).include?(key)
   end
 
   def set(key, val)
+    if include?(key) 
+      bucket(key).update(key,val) 
+    else
+      bucket(key).append(key,val)
+      @count += 1
+      resize! if @count >= @store.length 
+    end
   end
 
   def get(key)
+    bucket(key).get(key)
   end
 
   def delete(key)
+    @count -= 1 if bucket(key).remove(key) != nil 
   end
 
-  def each
+  def each(&prc)
+    @store.each do |list|
+      list.each do |node|
+        prc.call(node.key,node.val)
+      end 
+    end
   end
 
   # uncomment when you have Enumerable included
@@ -41,9 +58,21 @@ class HashMap
   end
 
   def resize!
+    temp_store = Array.new(num_buckets * 2) { LinkedList.new }
+    @store.each do |sub_arr|
+      sub_arr.each do |ele|
+        if temp_store[ele.hash % temp_store.length].include?(ele) 
+          temp_store[ele.hash % temp_store.length].update(ele,ele.val) 
+        else
+          temp_store[ele.hash % temp_store.length].append(ele,ele.val)
+        end
+      end
+    end
+    @store = temp_store
   end
 
   def bucket(key)
+    @store[key.hash % num_buckets]
     # optional but useful; return the bucket corresponding to `key`
   end
 end
