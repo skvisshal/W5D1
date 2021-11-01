94a1575ac2d7eb18a4fd87d4d4a71fde29772b2c
AndyAYu <andyhkgw@gmail.com>
2021-11-01 15:28:12 -0400

    finish p02

--- visshal_andy_hash_map_project/lib/p02_hashing.rb
+++ visshal_andy_hash_map_project/lib/p02_hashing.rb
@@ -3,19 +3,38 @@ class Integer
 end
 
 class Array
+
   def hash
+    sum = 0
+    self.each_with_index do |ele,i|
+      sum += ele^i
+    end
+    sum
   end
+
 end
 
 class String
+
   def hash
+    sum= 0
+    self.each_char.with_index do |char,i|
+      sum += char.ord^i
+    end
+    sum
   end
+
 end
 
 class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
+
   def hash
-    0
+    sum = 0
+    self.each do |key,value|
+      sum += (key.object_id)^value.hash
+    end
+    sum
   end
 end
